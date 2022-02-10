761535d4542d2886ea72031f55cbb3372be17d2b
Alim Mamutov <mamutov.creation@yandex.ru>
2022-02-10 17:46:21 +0300

    Смог понять как подключить два js для корзины и заказа, чтобы одни не перезатирал другой

--- static/js/order_scripts.js
+++ static/js/order_scripts.js
@@ -1,3 +1,8 @@
 function orders() {
+    let quantity, price, orderitem_num, delta_quantity, orderitem_quantity, delta_cost
+    let quantity_arr = []
+    let price_arr = []
+
+    let total_forms = parseInt($('input[name=orderitems-TOTAL-FORMS]').val())
 }
 window.addEventListener('DOMContentLoaded', orders);
