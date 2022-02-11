010d036e8d4ce1a86477aed09ced70cbf0ee6c77
Alim Mamutov <mamutov.creation@yandex.ru>
2022-02-11 13:02:34 +0300

    order_scripts.js

--- static/js/order_scripts.js
+++ static/js/order_scripts.js
@@ -3,7 +3,9 @@ function orders() {
     let quantity_arr = []
     let price_arr = []
 
-    let total_forms = parseInt($('input[name=orderitems-TOTAL_FORMS]').val());
-    console.log(dir(total_forms));
+    let total_forms = parseInt($('input[name=orderitems-TOTAL_FORMS]').val()); // Здесь получаем количество строк таблицы
+    // $ - это функция поиска из библиотеки jQuery, можно так же вызвать - jQuery('здесь указываем что ищем - тег[параметры тега]')
+    console.log(total_forms);
+     let total_quantity = parseInt($(
 }
 window.addEventListener('DOMContentLoaded', orders);
