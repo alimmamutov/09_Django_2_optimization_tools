ae8c1969aff2fe86fb33a98094066c25613c5656
Alim Mamutov <mamutov.creation@yandex.ru>
2022-02-11 12:09:17 +0300

    order_scripts.js

--- static/js/order_scripts.js
+++ static/js/order_scripts.js
@@ -3,6 +3,7 @@ function orders() {
     let quantity_arr = []
     let price_arr = []
 
-    let total_forms = parseInt($('input[name=orderitems-TOTAL-FORMS]').val())
+    let total_forms = parseInt($('input[name=orderitems-TOTAL_FORMS]').val());
+    console.log(dir(total_forms));
 }
 window.addEventListener('DOMContentLoaded', orders);
